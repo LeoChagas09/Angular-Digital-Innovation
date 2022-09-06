502e5ed6090b96e6bb1a2e76e817cd926a6a2e55
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2022-09-06 13:06:29 -0300

    Trabalhando com delete

--- Angular/src/app/courses/course-list.component.ts
+++ Angular/src/app/courses/course-list.component.ts
@@ -31,6 +31,16 @@ export class CourseListComponent implements OnInit {
 
   }
 
+  deleteById(courseId: number): void {
+    this.courseService.deleteById(courseId).subscribe({
+      next: () => {
+        console.log('Deleted with success');
+        this.retriveAll();
+      },
+      error: erro => console.log('Error', erro)
+    })
+  }
+
   set filter(value: string) {
       this._filterBy = value;
 
