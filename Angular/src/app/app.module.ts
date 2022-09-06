7e5688243bcb3685c0d8252486e6c6e7f784390a
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2022-09-06 10:59:14 -0300

    Lidando com vários commponentes
    Protegendo Rotas com Guards
    Trabalhando com formulários e templates
    ativando Rotas para acessar componentes
    e entre outras aulas

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,20 +1,46 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { Error404Compoennt } from './404/error-404.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseInfoComponent } from './courses/course-info.compenent';
 import { CourseListComponent } from './courses/course-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 import { StarComponent } from './star/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    NavBarComponent,
+    Error404Compoennt,
+    ReplacePipe,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+
+      {
+        path: '**', component: Error404Compoennt
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
