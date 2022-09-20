d3da7c0f4e7d19f46a8686c61a7be717c3536fdb
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2022-09-20 09:31:26 -0300

    Criando um formulário

--- TecnicasAvançadasAngular8/src/app/produtos/cadastro-produtos/cadastro-produtos.component.ts
+++ TecnicasAvançadasAngular8/src/app/produtos/cadastro-produtos/cadastro-produtos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'dio-cadastro-produtos',
@@ -8,17 +8,34 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class CadastroProdutosComponent implements OnInit {
 
-  options: FormGroup;
+  cadastro: FormGroup;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
 
-    this.options = this.fb.group({
-      hideRequired: false,
-      floatLabel: 'auto',
+    this.cadastro = this.fb.group({
+      nomeProduto: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
+      urlFoto: ['', [Validators.minLength(10)]],
+      dtLancamento: ['', [Validators.required]],
+      descricao: [''],
+      teorAlcoolico: [0, [Validators.required, Validators.min(0), Validators.max(100)]],
+      embalagens: [''],
+      tipo: ['', [Validators.required]],
     });
 
+  }
+
+  salvar(): void {
+    if(this.cadastro.invalid) {
+      return;
+    }
+
+    alert('SUCESSO!!\n\n' + JSON.stringify(this.cadastro.value, null, 4))
+  }
+
+  reiniciarForm(): void {
+    this.cadastro.reset();
   }
 
 }
